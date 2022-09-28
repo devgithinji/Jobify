6244907a2f39f0ae3d1625edd89edbab8f357924
dennis githinji <wakahiad@gmail.com>
2022-09-28 18:35:04 +0300

    dashboard stats

--- client/src/components/ChartsContainer.js
+++ client/src/components/ChartsContainer.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Wrapper from "../assets/wrappers/ChartsContainer";
+import {useAppContext} from "../context/appContext";
+import AreaChartComponent from "./AreaChartComponent";
+import BarChart from "./BarChartComponenet";
 
 const ChartsContainer = () => {
+    const [barChart, setBarChart] = useState();
+    const {monthlyApplications: data} = useAppContext();
+
     return (
-        <div>
-            charts container
-        </div>
+        <Wrapper>
+            <h4>Monthly Applications</h4>
+            <button type='button' onClick={() => setBarChart(!barChart)}>
+                {barChart ? 'AreaChartComponent' : 'BarChart'}
+            </button>
+            {barChart ? <BarChart data={data}/> : <AreaChartComponent data={data}/>}
+        </Wrapper>
     );
 };
 
